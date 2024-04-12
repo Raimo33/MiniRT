8afbd66014c881b4e58d0c58fbdb3f6cca1f80bd
Claudio Raimondi <craimond@c1r3p2.42firenze.it>
2024-04-12 16:45:27 +0200

    Fix bug in calculate_specular (vec not normalized)

--- srcs/render_light.c
+++ srcs/render_light.c
@@ -6,7 +6,7 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 14:30:16 by craimond          #+#    #+#             */
-/*   Updated: 2024/04/12 15:56:15 by craimond         ###   ########.fr       */
+/*   Updated: 2024/04/12 16:44:25 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,11 +93,13 @@ static double	calculate_specular(const t_vector view_dir, const t_vector light_d
 {
 	t_vector		reflected_light_dir;
 	const double	dot_nl = fmax(vec_dot(surface_normal, light_dir), 0.0f);
+	const double	dot_nl2 = 2.0f * dot_nl;
 
-	reflected_light_dir.x = 2.0f * dot_nl * surface_normal.x - light_dir.x;
-	reflected_light_dir.y = 2.0f * dot_nl * surface_normal.y - light_dir.y;
-	reflected_light_dir.z = 2.0f * dot_nl * surface_normal.z - light_dir.z;
-	
+	reflected_light_dir.x = dot_nl2 * surface_normal.x - light_dir.x;
+	reflected_light_dir.y = dot_nl2 * surface_normal.y - light_dir.y;
+	reflected_light_dir.z = dot_nl2 * surface_normal.z - light_dir.z;
+
+	reflected_light_dir = vec_normalize(reflected_light_dir);
 	const double spec_angle = fmax(vec_dot(reflected_light_dir, view_dir), 0.0f);
 	return (pow(spec_angle, shininess));
 }
